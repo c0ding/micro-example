33b29eaee9fedb29e8b554c3700e84eb6824b5d1
zyy <zhangyiyanghz@gmail.com>
2019-09-28 15:56:07 +0800

    fix

--- prepare/cobra_usage/cmd/root.go
+++ prepare/cobra_usage/cmd/root.go
@@ -32,11 +32,7 @@ var rootCmd = &cobra.Command{
 	Use:   "demo1",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
@@ -58,6 +54,6 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().StringVarP(&name, "name", "n", "", "人的名字")
+	rootCmd.Flags().StringVarP(&name, "name", "n", "无名氏", "人的名字")
 	rootCmd.Flags().IntVarP(&age, "age", "a", 0, "一个人的年龄")
 }
--- prepare/cobra_usage/impl/show.go
+++ prepare/cobra_usage/impl/show.go
@@ -3,5 +3,5 @@ package impl
 import "fmt"
 
 func Show(name string, age int) {
-	fmt.Printf("my name is %s, my age is %d", name, age)
+	fmt.Printf("my name is %s, my age is %d \n", name, age)
 }
